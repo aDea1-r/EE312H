f80e05d68f615c974416f6f94c8f1bbab2b821e7
aDea1-r <adeel2001@live.com>
2020-02-18 15:42:35 -0600

    Moved Starter Files

---
+++ Project 2/grading/submissions/infinite_loop.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "MatrixMultiply.h"
+
+void multiplyMatrices(
+        double a[],
+        const uint32_t a_rows,
+        const uint32_t a_cols,
+        double b[],
+        const uint32_t b_cols,
+        double c[]) {
+    while(1);
+}
+
+double** multiplyMatricesPtr(
+        double** a,
+        const uint32_t a_rows,
+        const uint32_t a_cols,
+        double** b,
+        const uint32_t b_cols) {
+    while(1);
+    return 0;
+}
+
+double** transposeMatrix(
+         double** a,
+         const uint32_t a_rows,
+         const uint32_t a_cols) {
+    while(1);
+    return 0;
+}
