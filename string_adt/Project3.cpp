23bd0b74eb662770dde02d2f32c0727da192dc44
aDea1-r <adeel2001@live.com>
2020-02-26 17:34:04 -0600

    Wrote Project 3 Beta Code

--- string_adt/Project3.cpp
+++ string_adt/Project3.cpp
@@ -1,7 +1,7 @@
 // String ADT Project3.cpp
 // EE 312 Project 3 submission by
 // Adeel Rehman
-// Your EID
+// aur228
 // Slip days used: 0
 // Spring 2020
 // Copy and paste this file at the top of all your submitted source code files.  Do not turn this in by itself.
@@ -35,7 +35,21 @@ bool isOurs(const UTString* s) {
  * Return a pointer to the UTString.
  */
 UTString* utstrdup(const char* src) {
-    return NULL;
+    UTString *s = (UTString*) malloc(sizeof(UTString));
+    uint32_t i;
+    for(i=0; src[i] == 0; i++);
+
+    s->length = i;
+    if (i<11)
+        i=11;
+    s->string = (char*) malloc(i* sizeof(char) + 5); //add 1 for /0, 4 for terminator
+    s->capacity = i;
+
+    for(i=0; i<src[i] + 1; i++) {
+        s->string[i] = src[i];
+    }
+    *((uint32_t*) &(s->string[i]) ) = SIGNATURE;
+
 }
 
 /*
@@ -43,7 +57,7 @@ UTString* utstrdup(const char* src) {
  *  s must be a valid UTString.
  */
 uint32_t utstrlen(const UTString* s) {
-    return NULL;
+    return (*s).length;
 }
 
 /*
@@ -54,7 +68,18 @@ uint32_t utstrlen(const UTString* s) {
  * Update the length of s.
  * Return s with the above changes. */
 UTString* utstrcat(UTString* s, const char* suffix) {
-    return NULL;
+    uint32_t i;
+    uint32_t prevLength = s->length;
+    for(i = s->length; i < s->capacity && suffix[i-prevLength]; i++ ) {
+        s->string[i] = suffix[i- prevLength];
+        s->length++;
+    }
+    s->string[i] = 0;
+    *((uint32_t*) &(s->string[i+1]) ) = SIGNATURE;
+
+    return s;
+
+
 }
 
 /*
@@ -67,14 +92,22 @@ UTString* utstrcat(UTString* s, const char* suffix) {
  * Return dst with the above changes.
  */
 UTString* utstrcpy(UTString* dst, const char* src) {
-    return NULL;
+    uint32_t i = 0;
+    for(i=0; i<dst->capacity && src[i]; i++) {
+        dst->string[i] = src[i];
+    }
+    dst->string[i] = 0;
+    *((uint32_t*) &(dst->string[i+1]) ) = SIGNATURE;
+
+    return dst;
 }
 
 /*
  * Free all memory associated with the given UTString.
  */
 void utstrfree(UTString* self) {
-
+    free(self->string);
+    free(self);
 }
 
 /*
@@ -87,5 +120,7 @@ void utstrfree(UTString* self) {
  * Return s with the above changes.
  */
 UTString* utstrrealloc(UTString* s, uint32_t new_capacity) {
-    return NULL;
+    if(s->capacity < new_capacity) {
+        s->string = (char*)realloc(s->string, new_capacity * sizeof(char));
+    }
 }
