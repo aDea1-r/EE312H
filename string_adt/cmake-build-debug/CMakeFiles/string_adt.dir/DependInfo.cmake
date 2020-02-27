674f934665ee144cb903984e5f25987ccaf8e050
aDea1-r <adeel2001@live.com>
2020-02-26 19:50:54 -0600

    Added Clion files

---
+++ string_adt/cmake-build-debug/CMakeFiles/string_adt.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/string_adt/Project3.cpp" "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/string_adt/cmake-build-debug/CMakeFiles/string_adt.dir/Project3.cpp.o"
+  "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/string_adt/main.cpp" "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/string_adt/cmake-build-debug/CMakeFiles/string_adt.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ string_adt/cmake-build-debug/CMakeFiles/string_adt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/string_adt.dir/Project3.cpp.o"
+  "CMakeFiles/string_adt.dir/main.cpp.o"
+  "libstring_adt.dll.a"
+  "string_adt.exe"
+  "string_adt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/string_adt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
