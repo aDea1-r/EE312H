bf98e79bca8c1c0616eca8a037a2b6bd322c6357
aDea1-r <adeel2001@live.com>
2020-03-09 18:35:02 -0500

    Added starter code

---
+++ CRM/cmake-build-debug/CMakeFiles/CRM.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CRM.dir/MyString.cpp.o"
+  "CMakeFiles/CRM.dir/Project4.cpp.o"
+  "CMakeFiles/CRM.dir/main.cpp.o"
+  "CRM.exe"
+  "CRM.pdb"
+  "libCRM.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CRM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
