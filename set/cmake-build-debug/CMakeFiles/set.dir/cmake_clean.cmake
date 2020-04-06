89fbce80cadb74f08d80f661e30b75efbefb2068
aDea1-r <adeel2001@live.com>
2020-04-06 06:35:41 -0500

    Lab 5 working on windows

---
+++ set/cmake-build-debug/CMakeFiles/set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set.dir/Project5.cpp.o"
+  "CMakeFiles/set.dir/main.cpp.o"
+  "libset.dll.a"
+  "set.exe"
+  "set.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ set/sample_grading_script/submissions/runtime_error.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Set.h"
+
+void destroySet(Set* self) {
+    int *x = 0; *x = 1;
+}
+
+void createEmptySet(Set* self) {
+    int *x = 0; *x = 1;
+}
+
+void createSingletonSet(Set* self, int x) {
+    int *x = 0; *x = 1;
+}
+
+void createCopySet(Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+}
+
+void assignSet(Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+}
+
+bool isMemberSet(const Set* self, int x) {
+    int *x = 0; *x = 1;
+    return false;
+}
+
+void insertSet(Set* self, int x) {
+    int *x = 0; *x = 1;
+}
+
+void removeSet(Set* self, int x) {
+    int *x = 0; *x = 1;
+}
+
+void displaySet(const Set* self) {
+    int *x = 0; *x = 1;
+}
+
+bool isEqualToSet(const Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+    return false;
+}
+
+bool isSubsetOf(const Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+    return false;
+}
+
+bool isEmptySet(const Set* self) {
+    int *x = 0; *x = 1;
+    return false;
+}
+
+void intersectFromSet(Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+}
+
+void subtractFromSet(Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+}
+
+void unionInSet(Set* self, const Set* other) {
+    int *x = 0; *x = 1;
+}
