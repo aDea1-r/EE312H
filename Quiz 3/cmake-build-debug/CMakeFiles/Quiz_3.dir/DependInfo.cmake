ad87145a80924e77a78f1d3df1bc98558ca3552c
aDea1-r <adeel2001@live.com>
2020-04-13 14:50:28 -0500

    Finished Recursive Sum Quiz

--- Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/DependInfo.cmake
+++ Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/DependInfo.cmake
@@ -1,15 +1,15 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
+  "C"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/Quiz 3/main.cpp" "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/main.cpp.o"
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/Quiz 3/main.c" "/cygdrive/c/Users/adeel/CLionProjects/EE 312H Labs/Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/main.c.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
--- Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/cmake_clean.cmake
+++ Quiz 3/cmake-build-debug/CMakeFiles/Quiz_3.dir/cmake_clean.cmake
@@ -1,11 +1,11 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Quiz_3.dir/main.cpp.o"
+  "CMakeFiles/Quiz_3.dir/main.c.o"
   "Quiz_3.exe"
   "Quiz_3.pdb"
   "libQuiz_3.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/Quiz_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
